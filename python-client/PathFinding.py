96ef09b79a35807e8f2aaec1c0ea79005e10a137
Nathaniel Simard <nathaniel@localhost.localdomain>
2017-01-07 11:03:30 -0500

    on try et on except

--- python-client/PathFinding.py
+++ python-client/PathFinding.py
@@ -5,14 +5,18 @@ class PathFinding:
     def __init__(self):
         self.path = "http://game.blitz.codes:8081/pathfinding/direction"
     def getPath(self, theMap, size, start, target):
-        start_f = "(" + str(start["x"]) + "," + str(start["y"]) + ")"
-        target_f = "(" + str(target[0]) + "," + str(target[1]) + ")"
-        url = ""
-        url += self.path + "?size=" + str(size)
-        url += "&start=" + start_f
-        url += "&target=" + target_f +"&"
-        theRealMap = {}
-        theRealMap["map"] = theMap["tiles"]
-        url +=  urllib.parse.urlencode(theRealMap)
-        response = requests.get(url)
-        return response.json()["direction"]
+        try:
+            start_f = "(" + str(start["x"]) + "," + str(start["y"]) + ")"
+            target_f = "(" + str(target[0]) + "," + str(target[1]) + ")"
+            url = ""
+            url += self.path + "?size=" + str(size)
+            url += "&start=" + start_f
+            url += "&target=" + target_f +"&"
+            theRealMap = {}
+            theRealMap["map"] = theMap["tiles"]
+            url +=  urllib.parse.urlencode(theRealMap)
+            response = requests.get(url)
+            return response.json()["direction"]
+        except:
+            print("Alert shit happened")
+            return 'Stay'
