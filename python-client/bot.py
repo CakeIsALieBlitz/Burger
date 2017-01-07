5fe9423f7c15fbf855f65339774d3fdd1a8ab584
Nathaniel Simard <nathaniel@localhost.localdomain>
2017-01-07 14:06:15 -0500

    sbdjhfvsjdh

--- python-client/bot.py
+++ python-client/bot.py
@@ -32,6 +32,7 @@ class SimpleBot(Bot):
 
     def move(self, state):
         game = Game(state)
+        self.updateOrder(game)
         if(self.isFirstTime):
             self.customer = game.customers[0]
             self.isFirstTime = False
@@ -46,7 +47,6 @@ class SimpleBot(Bot):
         else:
             self.updateClient(game)
 
-
         return self.__getDirection(game, self.target, game.myHero.pos)
 
     def __getDirection(self, game, target, start):
@@ -55,31 +55,33 @@ class SimpleBot(Bot):
         return self.pathFinder.getPath(theMap, size, start, target)
 
     def updateClient(self, game):
-        print("updateClient")
         if(game.myHero.nbBurger < self.nbBurger):
             self.getNewTarget(game)
         if(game.myHero.nbFrittes < self.nbFries):
             self.getNewTarget(game)
         if(self.calories < game.myHero.calories):
-            print("on a un customer")
             self.getNewTarget(game)
 
     def updateFries(self, game):
-        print("updateFries")
         if(game.myHero.nbFrittes < self.nbFries):
-            print("voller")
             self.getNewTarget(game)
         elif(game.myHero.nbFrittes > self.nbFries):
-            print("on a la fries")
             self.getNewTarget(game)
 
+    def updateOrder(self, game):
+        if(not self.isFirstTime):
+            nowOrder = game.customers[self.nbCustomer]
+            if(self.customer.burger != nowOrder.burger):
+                self.getNewTarget(game)
+                self.customer = game.customers[self.nbCustomer]
+            if(self.customer.french_fries != nowOrder.french_fries):
+                self.getNewTarget(game)
+                self.customer = game.customers[self.nbCustomer]
+
     def updateBurger(self, game):
-        print("updateBurger")
         if(game.myHero.nbBurger < self.nbBurger):
-            print("voller")
             self.getNewTarget(game)
         elif(game.myHero.nbBurger > self.nbBurger):
-            print("on a le burger")
             self.getNewTarget(game)
 
     def getNewTarget(self, game):
@@ -94,7 +96,7 @@ class SimpleBot(Bot):
         self.nbFries = 0
         self.nbBurger =0
         self.calories = game.myHero.calories
-        self.nbCustomer += 1
+        self.nbCustomer += 0
         self.current_target = "Client"
         self.customer = game.customers[self.nbCustomer]
         self.target = self.customer.pos
